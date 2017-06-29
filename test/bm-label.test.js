649bd8a53df0f593b22a2f38311c28db989b25db
Saravanakumar <sanatar2@cisco.com>
2017-06-29 16:47:41 +0530

    make it better

--- test/bm-label.test.js
+++ test/bm-label.test.js
@@ -1,10 +1,29 @@
 var assert = require('assert');
 let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server/server');
+let should = chai.should();
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+chai.use(chaiHttp);
+
+  describe('index / route', () => {
+    it('should return valid page results', (done) => {
+      chai.request(server).get('/').end((err, res) => {
+        res.should.have.status(200);
+        // res.body.should.be.a('text');
+        // res.should.equal('bar');
+        done();
+      })
     });
   });
-});
+
+  describe('/bookmark route', () => {
+    it('should return valid main page results', (done) => {
+      chai.request(server).get('/bookmark').end((err, res) => {
+        res.should.have.status(200);
+        // res.body.should.be.a('text');
+        // res.should.equal('bar');
+        done();
+      })
+    });
+  });  
